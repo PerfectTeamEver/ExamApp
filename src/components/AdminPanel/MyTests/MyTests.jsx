40f78e16a0888f76d8266fb33211c2c58f1ee6f6
mirjahonoralov <mirjahonoralov@gmail.com>
2021-12-02 21:03:43 +0500

    show pushed tests

--- src/components/AdminPanel/MyTests/MyTests.jsx
+++ src/components/AdminPanel/MyTests/MyTests.jsx
@@ -2,26 +2,30 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { getCreatedTests } from "../../../store/questions/admin-slice";
-import { Back } from "./MyTests.style";
+import { Container, Back, Wrapper, CorrectAnswer } from "./MyTests.style";
 
 const MyTests = () => {
-   const { createdTests } = useSelector(getCreatedTests);
-   return (
-      <div>
-         <div>
-            <Link to="/admin">
-               <Back>Back</Back>
-            </Link>
-         </div>
-         <h1>Created Tests</h1>
-         <div>
-            <h1>Question:</h1>
-            {createdTests.map((test) => (
-               <h2>{test.question}</h2>
-            ))}
-         </div>
-      </div>
-   );
+  const { createdTests } = useSelector(getCreatedTests);
+  return (
+    <>
+      <Link to="/admin">
+        <Back>Back</Back>
+      </Link>
+      <Container>
+        <h1>Created Tests</h1>
+        {createdTests.map((test) => (
+          <Wrapper>
+            <h3>Question: {test.question}</h3>
+            Answers:
+            <CorrectAnswer>{test.correct_answer}</CorrectAnswer>
+            {test.incorrect_answers.map((answer) => {
+              return <p>{answer}</p>;
+            })}
+          </Wrapper>
+        ))}
+      </Container>
+    </>
+  );
 };
 
 export default MyTests;
--- src/components/AdminPanel/MyTests/MyTests.style.js
+++ src/components/AdminPanel/MyTests/MyTests.style.js
@@ -1,6 +1,42 @@
 import styled from "styled-components";
 
+export const Container = styled.div`
+  width: 100%;
+  padding: 50px 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+
+  h1 {
+    margin-bottom: 10px;
+  }
+`;
+
+export const Wrapper = styled.div`
+  width: 50%;
+  background-color: #303030;
+  padding: 30px 50px;
+  color: #fff;
+  margin: 10px 0;
+  h3 {
+    margin-bottom: 15px;
+  }
+  p {
+    font-size: 18px;
+    margin: 5px 0;
+    padding: 5px;
+  }
+`;
+
+export const CorrectAnswer = styled.p`
+  background-color: #7cfc00;
+  color: #000;
+`;
+
 export const Back = styled.button`
+  position: absolute;
   font-size: 18px;
   padding: 7px 15px;
   border: none;
@@ -9,4 +45,5 @@ export const Back = styled.button`
   color: #fff;
   margin: 10px;
   cursor: pointer;
+  z-index: 1;
 `;
